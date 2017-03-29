b4c5707adf546994eddd5efdf0aa87f4cb43d70f
iosonosempreio <tommaso.elli@gmail.com>
2017-03-29 16:16:49 +0200

    update

---
+++ s51n-slideshow.js
@@ -0,0 +1,30 @@
+function setSlideshow() {
+	var slidesPadding = 10;
+	var slideshows = $('.slideshow');
+	// console.log(slideshows);
+
+	slideshows.each(function(i, slideshow){
+		// console.log(slideshow);
+		var slides = $(slideshow).find('.slide');
+		var slideshowWidth = 0;
+
+		slides.each(function(i, slide){
+			// console.log(slide.clientWidth);
+			slideshowWidth += slide.clientWidth + slidesPadding;
+		})
+		
+		slideshowWidth -= 9; // remove the last padding, but leave 1px for breath.
+		//console.log('width',slideshowWidth);
+		$(slideshow).width(slideshowWidth);
+	});
+	console.log('slideshow set');
+}
+
+$( document ).ready(function(){
+	console.log('ready')
+	setTimeout(setSlideshow(), 500);
+});
+
+$( window ).resize(function() {
+  setSlideshow();
+});
